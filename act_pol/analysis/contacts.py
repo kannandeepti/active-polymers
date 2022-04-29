BUGFIX: contacts module distance calculation
CODE: rouse polymer with additional springs

act_pol/analysis/contacts.py
@@ -61,7 +61,7 @@ cmap_distance = 'magma'
 cmap_temps = 'coolwarm'
 cmap_contacts = "YlOrRd"
 
-def two_point_msd(simdir, ntraj, N=101, relative=None, squared=False):
+def two_point_msd(simdir, ntraj=None, N=101, relative=None, squared=False):
     """Compute mean squared distance between two beads on a polymer at
     a particular time point. Plot heatmap"""
     #TODO: average over structures and average over time
@@ -71,15 +71,19 @@ def two_point_msd(simdir, ntraj, N=101, relative=None, squared=False):
     metric = 'euclidean'
     if squared:
         metric = 'sqeuclidean'
+    if ntraj is None:
+        ntraj = get_ntraj(simdir)
     #ignore first half of tape (steady state) and then take time slices every 10 save points
     #to sample equilibrium structures
-    for j in range(ntraj):
-        X, t_save, D = process_sim(simdir / f'tape{j}.csv')
+    nreplicates = 0
+    for tape in simdir.glob('tape*.csv'):
+        j = tape.name[:-4][4:]
+        X, t_save, D = process_sim(tape)
         DT = np.diff(t_save)[0] #time between save points
         nrousetimes = int(np.ceil(350. / DT)) #number of frames that make up a rouse time
         ntimes, _, _ = X.shape
         #nreplicates = ntraj * (ntimes - 1)
-        nreplicates = ntraj * len(range(nrousetimes, ntimes, nrousetimes))
+        nreplicates += len(range(nrousetimes, ntimes, nrousetimes))
         if relative:
             Xeq, _, _ = process_sim(Path(relative) / f'tape{j}.csv')
         for i in range(nrousetimes, ntimes, nrousetimes):
@@ -100,9 +104,9 @@ def two_point_msd(simdir, ntraj, N=101, relative=None, squared=False):
 
     if relative:
         eq_dist = eq_dist / nreplicates
-        return Rg_squared, average_dist, eq_dist
+        return Rg_squared, average_dist, eq_dist, nreplicates
 
-    return Rg_squared, average_dist
+    return Rg_squared, average_dist, nreplicates
 
 def compute_relative_change(dist, eqdist):
     dist1 = dist
@@ -144,12 +148,15 @@ def plot_msd_map(dist, simdir, relative=False, squared=False):
         plt.savefig(f'plots/two_point_msd_{simdir.name}.pdf')
 
 def heatmap_divider(mat, temps, simname, relative=False, relative_change=False,
-                    width=5, lines=None, marks=None, **kwargs):
+                    width=5, lines=None, marks=None, nreplicates=None, **kwargs):
     """ Plot matrix of mean separations between monomers (i,j) with color bars
      showing temperature of beads."""
     if mat is None:
         mat = np.random.randn((101, 101))
-
+    if nreplicates:
+        nreps = f', n={nreplicates}'
+    else:
+        nreps = ''
     D = np.ones((width, 101))
     for i in range(width):
         D[i, :] = temps
@@ -182,11 +189,12 @@ def heatmap_divider(mat, temps, simname, relative=False, relative_change=False,
             x, y = mark
             ax.plot(x, y, "X", color='white')
     if relative:
-        ax.set_title(r'$\langle r_{ij} \rangle  - \langle r_{ij} \rangle_{eq}$')
+        title = (r'$\langle r_{ij} \rangle  - \langle r_{ij} \rangle_{eq}$')
     elif relative_change:
-        ax.set_title(r'Relative \% change in $\langle r_{ij} \rangle$')
+        title = (r'Relative \% change in $\langle r_{ij} \rangle$')
     else:
-        ax.set_title(r'Mean distance $\langle r_{ij} \rangle$')
+        title = (r'Mean distance $\langle r_{ij} \rangle$')
+    ax.set_title(title + nreps)
     ax_bottom.set_xlabel(r'Bead $i$')
     ax_left.set_ylabel(r'Bead $j$')
     fig.tight_layout()
@@ -239,7 +247,7 @@ def mdmap_abs_rel(dist, eqdist, temps, simname, relative=False, width=5, **kwarg
     fig.tight_layout()
     plt.savefig(f'plots/two_point_msd_{simname}_rel_front.pdf')
 
-def contact_probability(a, simdir, ntraj, N=101, eq_contacts=None):
+def contact_probability(a, simdir, N=101, eq_contacts=None):
     """Compute mean squared distance between two beads on a polymer at
     a particular time point. Plot heatmap"""
     #TODO: average over structures and average over time
@@ -250,12 +258,14 @@ def contact_probability(a, simdir, ntraj, N=101, eq_contacts=None):
     metric = 'euclidean'
     #ignore first half of tape (steady state) and then take time slices every 10 save points
     #to sample equilibrium structures
-    for j in range(ntraj):
+    nreplicates = 0
+    for tape in simdir.glob('tape*.csv'):
+        j = tape.name[:-4][4:]
         X, t_save, D = process_sim(simdir / f'tape{j}.csv')
         DT = np.diff(t_save)[0]  # time between save points
         nrousetimes = int(np.ceil(350. / DT))  # number of frames that make up a rouse time
         ntimes, _, _ = X.shape
-        nreplicates = ntraj * len(range(nrousetimes, ntimes, nrousetimes))
+        nreplicates += len(range(nrousetimes, ntimes, nrousetimes))
         if eq_contacts:
             Xeq, _, _ = process_sim(Path('csvs/bdeq1') / f'tape{j}.csv')
         for i in range(nrousetimes, ntimes, nrousetimes):
@@ -274,7 +284,7 @@ def plot_contact_map(contacts, eq_contacts = None, robust=True, **kwargs):
                     cmap="vlag", xticklabels=25, yticklabels=25, robust=True, ax=ax)
         ax.set_title(r'Contact map relative to equilibrium')
     else:
-        contacts[contacts == 0] = 1e-5
+        contacts[contacts == 0] = np.min(contacts[contacts > 0])/2.0
         lognorm = LogNorm(vmin=contacts.min(), vmax=contacts.max())
         res = sns.heatmap(contacts, norm=lognorm,
                     cmap="Reds", square=True, xticklabels=25, yticklabels=25, robust=robust, ax=ax,
@@ -289,10 +299,15 @@ def plot_contact_map(contacts, eq_contacts = None, robust=True, **kwargs):
     plt.savefig(f'plots/contact_map_{simdir.name}.pdf')
     plt.show()
 
-def plot_contact_map_temps(contacts, temps, simname, width=5, a=1, tag=None, **kwargs):
+def plot_contact_map_temps(contacts, temps, simname, width=5, a=1, tag=None,
+                           nreplicates=None, **kwargs):
     if tag is None:
         tag = ''
-    contacts[contacts == 0] = 1e-5
+    if nreplicates:
+        nreps = f', n={nreplicates}'
+    else:
+        nreps = ''
+    contacts[contacts == 0] = np.min(contacts[contacts > 0])/2.0
     lognorm = LogNorm(vmin=contacts.min(), vmax=contacts.max())
     D = np.ones((width, 101))
     for i in range(width):
@@ -308,15 +323,12 @@ def plot_contact_map_temps(contacts, temps, simname, width=5, a=1, tag=None, **k
     ax_left.set_yticks([0, 25, 50, 75, 100])
     ax_left.set_xticks([])
     ax.set_xticks([])
-
-
-
     ax.set_yticks([])
     im = ax.imshow(contacts, norm=lognorm, cmap=cmap_contacts, **kwargs)
     ax_bottom.imshow(D, cmap='coolwarm', vmin=0.25, vmax=1.75)
     ax_left.imshow(D.T, cmap='coolwarm', vmin=0.25, vmax=1.75)
     fig.colorbar(im, cax=cax)
-    ax.set_title(f'Contact Map $P(\\Vert \\vec{{r}}_i - \\vec{{r}}_j \\Vert < {a})$')
+    ax.set_title(f'Contact Map $P(\\Vert \\vec{{r}}_i - \\vec{{r}}_j \\Vert < {a})$' + nreps)
     ax_bottom.set_xlabel(r'Bead $i$')
     ax_left.set_ylabel(r'Bead $j$')
     fig.tight_layout()
@@ -372,15 +384,17 @@ def vary_contact_radius(sims, radii):
             counts, contacts = contact_probability(a, f'csvs/{sim}', 96)
             plot_contact_map_temps(contacts, Ds, sim, a=a, tag=f'_a{a}')
 
-def distance_distribution(ind1, ind2, simdir, ntraj):
+def distance_distribution(ind1, ind2, simdir):
     simdir = Path(simdir)
     distances = []
-    for j in range(ntraj):
+    nreplicates = 0
+    for tape in simdir.glob('tape*.csv'):
+        j = tape.name[:-4][4:]
         X, t_save, D = process_sim(simdir / f'tape{j}.csv')
         ntimes, _, _ = X.shape
         DT = np.diff(t_save)[0] #time between save points
         nrousetimes = int(np.ceil(350. / DT)) #number of frames that make up a rouse time
-        nreplicates = ntraj * len(range(nrousetimes, ntimes, nrousetimes))
+        nreplicates += len(range(nrousetimes, ntimes, nrousetimes))
         for i in range(nrousetimes, ntimes, nrousetimes):
             #for temperature modulations
             distance = X[i, ind1, :] - X[i, ind2, :]
@@ -392,8 +406,8 @@ def plot_distance_distribution(i, j, simdir1, simdir2, label1, label2, mat):
     simdir1 = Path(simdir1)
     simname = simdir1.name
     simdir2 = Path(simdir2)
-    distances1 = distance_distribution(i, j, simdir1, 96)
-    distances2 = distance_distribution(i, j, simdir2, 96)
+    distances1 = distance_distribution(i, j, simdir1)
+    distances2 = distance_distribution(i, j, simdir2)
     print((np.mean(distances1) - np.mean(distances2))/np.mean(distances1))
     fig, ax = plt.subplots()
     n, bins, patches = ax.hist(distances1, bins='auto', density=True, histtype='step', label=label1)

act_pol/analysis/files.py
@@ -6,6 +6,7 @@ from pathlib import Path
 import subprocess
 import pandas as pd
 import matplotlib as mpl
+import glob
 
 def pull_down_data(simdirs, remote='dkannan@eofe8.mit.edu:~/git-remotes/active-polymers/csvs',
                    local='csvs'):
@@ -28,6 +29,12 @@ def pull_down_data(simdirs, remote='dkannan@eofe8.mit.edu:~/git-remotes/active-p
         localsim.mkdir() #this will raise a FileExistsError if directory already exists
         subprocess.run(f'rsync {remotesim}/*.csv {localsim}/')
 
+def get_ntraj(simdir):
+    """ Counts the number of files of the form tape*.csv -- aka number of independent simulation
+    trajectories that were run for this parameter set specified by simdir."""
+    tapes = glob.glob(str(simdir) + '/tape*.csv')
+    return len(tapes)
+
 def process_sim(file):
     df = pd.read_csv(file)
     dfg = df.groupby('t')
@@ -37,6 +44,7 @@ def process_sim(file):
     for t, mat in dfg:
         t_save.append(t)
         D.append(mat['D'].to_numpy())
+        #column 0 is time, columns 1-3 are x,y,z, and then column 4 is D
         X.append(mat.to_numpy()[:, 1:4])
     t_save = np.array(t_save)
     X = np.array(X)
@@ -63,7 +71,7 @@ def extract_cov(simdir, tape=0, time=350.0):
     return D, mat, rhos
 
 def extract_rhomat(simdir):
-    """ Extract contents of idmat.csv in simulation directory."""
+    """ Extract contents of rhomat.csv in simulation directory."""
     simdir = Path(simdir)
     simname = simdir.name
     if (simdir / 'rhomat.csv').is_file():

scripts/run_sim.py
@@ -31,6 +31,16 @@ def test_bd_sim(i, N=101, L=100, b=1, D=1):
     X, msd = with_srk1(N, L, b, np.tile(D, N), h, tmax, t_save=t_save, t_msd=t_msd)
     return X, msd, t_save
 
+def test_bd_loops(N=101, L=100, b=1, D=1):
+    h = 0.001
+    tmax = 350.0
+    K = np.array([[25, 75]])
+    t_save = np.linspace(0, tmax, 100 + 1)
+    t_msd = np.logspace(-2, 2, 10)
+    X, msd = loops_with_srk1(N, L, b, np.tile(D, N), h, tmax, K,
+                             t_save=t_save, t_msd=t_msd, msd_start_time=1.0)
+    return X, msd, t_save
+
 def test_init_avoid(N=101, L=100, b=1, D=1, a=0.2):
     """ Test initialization of particles that do not overlap."""
     d = 2.0 ** (1 / 6) * a
@@ -111,6 +121,38 @@ def test_corr_D(N=101, L=100, b=1, tmax=350.0, h=0.001):
     X, msd = correlated_amplitudes_srk2(N, L, b, rhos, D, h, tmax, t_save=t_save,
                                     t_msd=t_msd)
     return X, msd, t_save
+
+def run_loops(i, N, L, b, D, filedir, K=None):
+    """ Run a single simulation of a Rouse polymer with loops."""
+    file = Path(filedir) / f'tape{i}.csv'
+    msd_file = Path(filedir) / f'msds{i}.csv'
+    try:
+        file.parent.mkdir(parents=True)
+    except:
+        if file.parent.is_dir() is False:
+            # if the parent directory does not exist and mkdir still failed, re-raise an exception
+            raise
+    if not K:
+        K = np.array([[25, 75]])
+    h = 0.001
+    msd_start_time = 0.0
+    tmax = 1e4 + msd_start_time + h
+    t_save = 350.0 * np.arange(0, np.floor(tmax / 350.0) + 1)
+    t_msd = np.logspace(-2, 4, 86)
+    X, msd = loops_with_srk1(N, L, b, D, h, tmax, K,
+                             t_save=t_save, t_msd=t_msd, msd_start_time=msd_start_time)
+    dfs = []
+    for i in range(X.shape[0]):
+        df = pd.DataFrame(X[i, :, :])
+        df['t'] = t_save[i]
+        df['D'] = D  # save diffusivities of beads
+        dfs.append(df)
+    df = pd.concat(dfs, ignore_index=True, sort=False)
+    df.set_index(['t'], inplace=True)
+    df.to_csv(file)
+    df = pd.DataFrame(msd)
+    df['t_msd'] = t_msd
+    df.to_csv(msd_file)
 
 def run_correlated(i, N, L, b, D, filedir, length=10, 
                   s0=30, s0_prime=70, max=0.9):
@@ -326,7 +368,7 @@ if __name__ == '__main__':
     #D = np.tile(0.25, N)
     #D[10:30] = 1.75
     #D[50:80] = 1.75
-    filedir = Path('csvs/corrAmp')
+    filedir = Path('csvs/loops_25_75')
     #define cosine wave of temperature activity with amplitude 5 times equilibrium temperature
     #period of wave is 25, max is 11, min is 1
     #reduce time step by order of magnitude due to higher diffusivity
@@ -335,15 +377,15 @@ if __name__ == '__main__':
     #alternating hot and cold regions
     #simulation `1feat_rho.5_sameT`
     #Define hot to be 1.75 and cold to be 0.25 so mean D = 1.0
-    D = np.tile(0.5, N)
-    D[0:20] = 1.5
-    D[40:60] = 1.5
-    D[80:] = 1.5
-    mat = np.zeros((1, N))
-    mat[0, 0:20] = -0.5
-    mat[0, 40:60] = 0.5
-    mat[0, 80:] = 0.5
-
+    #D = np.tile(0.5, N)
+    #D[0:20] = 1.5
+    #D[40:60] = 1.5
+    #D[80:] = 1.5
+    #mat = np.zeros((1, N))
+    #mat[0, 0:20] = -0.5
+    #mat[0, 40:60] = 0.5
+    #mat[0, 80:] = 0.5
+    """
     file = Path(filedir)/'rhomat.csv'
     try:
         file.parent.mkdir(parents=True)
@@ -354,9 +396,10 @@ if __name__ == '__main__':
     #save identity matrix for future reference
     df = pd.DataFrame(mat)
     df.to_csv(file)
+    """
     
     print(f'Running simulation {filedir.name}')
-    func = partial(run_correlated_amplitudes, N=N, L=L, b=b, rhos=mat, D=D,
+    func = partial(run_loops, N=N, L=L, b=b, D=D,
                    filedir=filedir)
     tic = time.perf_counter()
     func(0)
@@ -366,5 +409,6 @@ if __name__ == '__main__':
     #    result = p.map(func, np.arange(0, N))
     toc = time.perf_counter()
     print(f'Ran simulation in {(toc - tic):0.4f}s')
+
 
 