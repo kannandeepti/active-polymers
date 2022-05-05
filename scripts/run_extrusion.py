9206a76d81daa602552a18f27a32405a660fb6be
Deepti Kannan <kannandeepti@yahoo.com>
2022-05-05 11:52:55 -0400

    CODE: typed argument parser for run script

--- scripts/run_extrusion.py
+++ scripts/run_extrusion.py
@@ -11,10 +11,15 @@ from functools import partial
 from scipy.spatial.distance import pdist, squareform
 import time
 import sys
+from tap import Tap
 
+class ExtrusionParams(Tap):
+    p: float #probability of looped state
+    sigma: float #nonequilibrium extent (ratio of time_looped to rouse time)
+    mean_loop_size: int #mean loop size in number of monomers
 
 def run_extrusion(i, N, L, b, D, p, sigma, mean_loop_size, filedir,
-                  s1=25, s2=75, h=0.001, desired_tmax=1e4, DT=35.0):
+                  s1=10, s2=90, h=0.001, desired_tmax=1e4, DT=35.0):
     """ Run a single simulation of a Rouse polymer with loops."""
     file = Path(filedir) / f'tape{i}.csv'
     msd_file = Path(filedir) / f'msds{i}.csv'
@@ -29,9 +34,6 @@ def run_extrusion(i, N, L, b, D, p, sigma, mean_loop_size, filedir,
     tmax = desired_tmax + msd_start_time + h
     t_save = DT * np.arange(0, np.floor(tmax / DT) + 1)
     t_msd = np.logspace(-2, 4, 86)
-    tmax = 100.0
-    t_save = np.linspace(0, tmax, 100 + 1)
-    t_msd = np.logspace(-2, 2, 10)
     mean_time_looped, mean_time_unlooped, vextrude = extrusion_parameters(p, sigma,
                                                                           mean_loop_size, b=b,
                                                                           D=D[0])
@@ -63,18 +65,19 @@ if __name__ == '__main__':
     L = 100
     b = 1
     D = 10 * np.ones(N)
-    p = sys.argv[1]
-    sigma = sys.argv[2]
-    mean_loop_size = sys.argv[3]
-    filedir = Path('csvs/extrusion_25_75')
+    args = ExtrusionParams().parse_args()
+    p = args.p
+    sigma = args.sigma
+    mean_loop_size = args.mean_loop_size
+    print(f'p={p}, sigma={sigma}, mean loop size={mean_loop_size}')
+    filedir = Path(f'csvs/extrusion/p{p}_sig{sigma}_loop{mean_loop_size}')
     print(f'Running simulation {filedir.name}')
     func = partial(run_extrusion, N=N, L=L, b=b, D=D, p=p, sigma=sigma, mean_loop_size=mean_loop_size,
                    filedir=filedir)
     tic = time.perf_counter()
-    func(0)
-    # pool_size = 16
-    # N = 96
-    # with Pool(pool_size) as p:
-    #    result = p.map(func, np.arange(0, N))
+    pool_size = 16
+    N = 96
+    with Pool(pool_size) as p:
+       result = p.map(func, np.arange(0, N))
     toc = time.perf_counter()
     print(f'Ran simulation in {(toc - tic):0.4f}s')
